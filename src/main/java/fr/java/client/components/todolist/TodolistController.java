e4c41da0c5ae914bd96fb3959b7cade730073574
henrimeng <henri.meng@gmail.com>
2021-05-09 17:42:02 +0200

    added delete list and add task for list

--- src/main/java/fr/java/client/components/todolist/TodolistController.java
+++ src/main/java/fr/java/client/components/todolist/TodolistController.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -22,6 +23,8 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class TodolistController {
 
@@ -44,23 +47,42 @@ public class TodolistController {
         this.refreshAction();
     }
 
+    public void displayCreateTaskView() throws IOException {
+        Stage stage = new Stage();
+        stage.setScene(FileUtils.createSceneFromFXLM("src/main/java/fr/java/client/components/createTask/createTask.fxml"));
+        stage.showAndWait();
+        this.refreshAction();
+    }
+
+    /**
+     * create a list in the listsHBox (TODOLIST)
+     * @param todolist
+     */
     public void addList(Todolist todolist) {
+
         Label titleLabel = new Label(todolist.getTitle());
         titleLabel.setStyle("-fx-font-weight: bolder; -fx-text-fill: #4e4b4b; -fx-padding: 10px;");
 
         Button btnAddTask = new Button();
         btnAddTask.setText("ADD TASK");
         btnAddTask.setCursor(Cursor.HAND);
-        btnAddTask.setStyle("-fx-background-color: transparent; -fx-border-color: transparent");
+        btnAddTask.getStyleClass().add("btnActionList");
         btnAddTask.setOnAction(actionEvent -> {
-            createTaskController createTask = new createTaskController();
             try {
-                createTask.createTaskAction();
+                this.displayCreateTaskView();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
 
+        Button btnDeleteList = new Button();
+        btnDeleteList.setText("DELETE LIST");
+        btnDeleteList.setCursor(Cursor.HAND);
+        btnDeleteList.getStyleClass().add("btnActionList");
+        btnDeleteList.setOnAction(e -> {
+            instance.getTodolistService().getTodolists().remove(todolist);
+            this.refreshAction();
+        });
 
 
         StackPane listHeader = new StackPane();
@@ -74,9 +96,11 @@ public class TodolistController {
         listBody.setStyle("-fx-background-color: #d0d0d0; -fx-spacing: 4px; -fx-padding: 5px; -fx-min-width: 210px;");
         addTask(todolist, listBody);
 
-
-        Pane listFooter = new Pane();
+        StackPane listFooter = new StackPane();
         listFooter.setStyle("-fx-background-color: #d0d0d0");
+        listFooter.getChildren().add(btnDeleteList);
+        listFooter.setAlignment(btnDeleteList, Pos.CENTER_RIGHT);
+
 
         VBox list = new VBox();
         list.getChildren().add(listHeader);
@@ -87,12 +111,30 @@ public class TodolistController {
         this.listsHBox.getChildren().add(list);
     }
 
+    /**
+     * create a simple card clickable for each task of a todolist (TASK)
+     * @param todolist
+     * @param listView
+     */
     public void addTask(Todolist todolist, VBox listView) {
         for (Task task : todolist.getTasks()) {
+
             TextFlow text = new TextFlow();
-            text.setStyle("-fx-padding: 10px; -fx-background-color: white;");
+            text.setStyle("-fx-padding: 10px; -fx-background-color: transparent;");
             text.getChildren().add(new Text(task.getContent()));
-            listView.getChildren().add(text);
+
+            StackPane card = new StackPane();
+
+            // remove himself on click
+            card.setOnMouseClicked(event -> {
+                todolist.getTasks().remove(task);
+                this.refreshAction();
+            });
+            card.getStyleClass().add("task");
+            card.setCursor(Cursor.HAND);
+
+            card.getChildren().add(text);
+            listView.getChildren().add(card);
         }
     }
 
