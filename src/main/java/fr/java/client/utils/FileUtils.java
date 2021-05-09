7ff5f6484a737e39d990d2feb261ea0399c02cbf
Mickael <akninemickael@gmail.com>
2021-05-09 18:35:42 +0200

    Ajout du fonction qui génère des alerte

--- src/main/java/fr/java/client/utils/FileUtils.java
+++ src/main/java/fr/java/client/utils/FileUtils.java
@@ -3,6 +3,7 @@ package fr.java.client.utils;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,5 +15,13 @@ public class FileUtils {
         URL file = new File(path).toURI().toURL();
         Parent root = FXMLLoader.load(file);
         return new Scene(root);
+    }
+
+
+    public static void showAlert(String header, String body, Alert.AlertType alertType) {
+        Alert errorAlert = new Alert(alertType);
+        errorAlert.setHeaderText(header);
+        errorAlert.setContentText(body);
+        errorAlert.showAndWait();
     }
 }
