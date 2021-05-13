b1fa7a9156ea5ea820b5ec02ddb97ab6dd80a792
ahmedine <krapulxx@gmail.com>
2021-05-13 23:44:08 +0200

    added features + view profile

--- src/main/java/fr/java/client/components/profil/ProfilController.java
+++ src/main/java/fr/java/client/components/profil/ProfilController.java
@@ -3,11 +3,14 @@ package fr.java.client.components.profil;
 import fr.java.client.entities.Todolist;
 import fr.java.client.entities.User;
 import fr.java.client.services.Instance;
+import fr.java.client.utils.FileUtils;
 import fr.java.client.utils.types.Roles;
 import fr.java.client.utils.types.TaskStatusType;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -15,19 +18,14 @@ public class ProfilController {
 
     Instance instance = Instance.getInstance();
 
-    @FXML
-    TextField usernameEntry;
-    @FXML
-    TextField passwordEntry;
-    @FXML
-    TextField aliasEntry;
-    @FXML
-    TextField ageEntry;
-    @FXML
-    TextField roleEntry;
-
-    @FXML
-    Button closeButton;
+    @FXML TextField usernameEntry;
+    @FXML PasswordField passwordEntry;
+    @FXML TextField aliasEntry;
+    @FXML TextField ageEntry;
+    @FXML TextField roleEntry;
+    @FXML Button closeButton;
+    @FXML CheckBox updateProfil;
+    @FXML Button saveBtn;
 
     @FXML
     protected void initialize() {
@@ -44,5 +42,28 @@ public class ProfilController {
     }
 
 
+    public void checkUpdateAccess() {
+        if(this.updateProfil.isSelected()) {
+            this.usernameEntry.setDisable(false);
+            this.passwordEntry.setDisable(false);
+            this.aliasEntry.setDisable(false);
+            this.ageEntry.setDisable(false);
+            this.saveBtn.setVisible(true);
+        } else {
+            this.usernameEntry.setDisable(true);
+            this.passwordEntry.setDisable(true);
+            this.aliasEntry.setDisable(true);
+            this.ageEntry.setDisable(true);
+            this.saveBtn.setVisible(false);
+        }
 
+    }
+
+    public void saveBtnAction() {
+        this.instance.getUserService().getUser().setUsername(this.usernameEntry.getText());
+        this.instance.getUserService().getUser().setPassword(this.passwordEntry.getText());
+        this.instance.getUserService().getUser().setAlias(this.aliasEntry.getText());
+        this.instance.getUserService().getUser().setAge(Integer.parseInt(this.ageEntry.getText()));
+        FileUtils.close(this.usernameEntry);
+    }
 }
